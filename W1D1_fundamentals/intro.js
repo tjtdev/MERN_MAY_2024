36f7965513c368446a9ffa8665fe1cf763fee6d5
JupiterOrbita <38020888+jupiterorbita@users.noreply.github.com>
2024-04-29 15:01:14 -0600

    add destructuring, rest, spread, arrow, ternary

--- W1D1_fundamentals/intro.js
+++ W1D1_fundamentals/intro.js
@@ -39,11 +39,14 @@ const person = {
     email: 'bob@marley.com',
     password: 'sekureP@ssw0rd9',
     username: 'barley',
-    createdAt: 1543945177623
+    createdAt: 1543945177623,
+    hobbies: ['ski', 'swim', 'gaming']
 };
-const email = person.email;
-const { email: objectEmail, firstName } = person;
+// const email = person.email;
+// const userName = person.username
+const { email: objectEmail, username, firstName, hobbies } = person;
 // console.log(objectEmail, email);
+console.log(objectEmail, username, firstName, hobbies[0]);
 
 
 
@@ -76,7 +79,72 @@ const personObj = {
 };
 
 const { addresses: [, { city }] } = personObj;
-console.log(city);
+// console.log(city);
 
 // console.log(personObj.addresses[0].city)
 
+const copyPerson = { ...personObj };
+
+
+
+
+//  --- spread operator ----
+
+const a = [{ a: 1, b: 2 }];
+const b = [...a];
+
+// Clone it
+const cloneA = structuredClone(a);
+
+a[0].a = 100;
+
+console.log(a); //?
+console.log(cloneA); //?
+
+// ---rest---
+function doStuff(num1, ...args) {
+    console.log(args);
+}
+
+doStuff(12, 23, 43, 54, 56, 67, 56);
+
+// ======= ARROW FUNCTIONS ========
+const hello = function (name) {
+    return "hello " + name;
+};
+hello("bob");
+
+const helloArrow = (name) => {
+    console.log("hello");
+    return "hello " + name;
+};
+helloArrow("Alice");
+
+const helloArrow2 = name => "hello " + name;
+
+const aPerson = {
+    name: 'John',
+    age: 30,
+    greetStandalone: function () {
+        console.log(`Hello, my name is ${this.name} and I'm ${this.age} years old.`);
+    },
+    greetArrow: () => {
+        console.log(`Hello, my name is ${this.name} and I'm ${this.age} years old.`);
+    }
+};
+aPerson.greetStandalone();
+aPerson.greetArrow();
+
+
+// === TERNARY ===
+const x = 10;
+const y = 20;
+const message = "this is a test";
+// if (x < y) {
+//     return "x is bigger";
+// } else {
+//     return "y is bigger";
+// }
+
+const result = message.length > 3 ? "<h1>here is your data</h1>" : <p>loading..</p>;
+console.log(result);
