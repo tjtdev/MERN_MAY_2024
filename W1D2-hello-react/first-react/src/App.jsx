3716197e0831c12f060c831ff9e304b0f6a0617e
JupiterOrbita <38020888+jupiterorbita@users.noreply.github.com>
2024-04-30 15:27:56 -0600

    add w1d2 intro to React

---
+++ W1D2-hello-react/first-react/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+
+  console.log("hello app.jsx");
+
+  return (
+    <>
+      <div>
+        <h1 className='red'>hello react</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, nesciunt.</p>
+
+      </div>
+    </>
+  );
+}
+
+export default App;
